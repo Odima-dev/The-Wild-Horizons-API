bf625fecb33e77707f1514de5fc958562acb6290
Odima-dev <vincentodima22@gmail.com>
2025-09-07 04:11:23 +0300

    Set up server

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-import http from 'http'
+import http from 'node:http'
 
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the Server!')
+})
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
